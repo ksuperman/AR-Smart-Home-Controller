662ec068c10318dc421fb6a4fcffb2ec420f06ce
Rakshith Koravadi Hatwar <rakshithk@live.com>
2017-04-23 19:14:58 -0700

    Header and Footer

---
+++ public/javascripts/initialize.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rakshithk on 4/23/17.
+ */
+$( document ).ready(function() {
+    $('#header').load('/templates/header.html', function() {
+        $(".header-nav").sideNav();
+    });
+
+    $('#sidenav').load('/templates/sidenav.html', function() {
+        $(".side-nav-button").sideNav();
+        $('.side-nav-container').sideNav();
+    });
+
+    $('#footer').load('/templates/footer.html');
+});
