fb2bf89da73b3f40a4109c6c21b302443f1d02a2
IyerVishSJSU <vishwanath.iyer@sjsu.edu>
2017-05-01 23:59:17 -0700

    Load Device History on Modals

--- public/javascripts/devdashboard.js
+++ public/javascripts/devdashboard.js
@@ -7,4 +7,73 @@ $(document).ready(function(){
     });
     $('#modal1').modal('open');
 
+    $.getJSON('/api/device/getAllDeviceHistory', function (data) {
+        console.log(myDevId);
+        var series = [];
+
+        for (var i=0 ; i< data.length; i++) {
+            if(myDevId===data[i].device_id){
+                var dayUsed = new Date(data[i].energy_date);
+                var date = moment(dayUsed, "MM/DD/YYYY HH:mm").unix()*1000;
+                series.push([date,data[i].energy_consumed]);
+            }
+        }
+
+        var chart = Highcharts.stockChart('highchart4', {
+
+            chart: {
+                height: 300
+            },
+
+            credits: {
+                enabled: true
+            },
+
+
+            title: {
+                text: hc4_title
+            },
+
+            yAxis: {
+                title: {
+                    text: 'KWatts'
+                }
+            },
+
+            rangeSelector: {
+                selected: 1
+            },
+
+            series: [{
+                name: hc4_subtitle,
+                data: series,
+                type: 'area',
+                threshold: null,
+                color:'#8cff66',
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        chart: {
+                            height: 300
+                        },
+                        subtitle: {
+                            text: null
+                        },
+                        navigator: {
+                            enabled: false
+                        }
+                    }
+                }]
+            }
+        });
+    });
+
 });
